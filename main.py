47a1f7141f7ecfda2b7e95cfaad51ea2b5792ed9
Bertrand <bertrand.buffat@epitech.eu>
2019-06-12 00:30:29 +0800

    added roc

--- main.py
+++ main.py
@@ -23,8 +23,16 @@ def split_data(sentences, labels):
             labels[int(split_frac*len_feat):])
 
 def read_data(params, data_dir="./data/", mlp=False):
-    embeddings = [ scipy.io.loadmat(data_dir + f)["data"].astype(np.float32)
-                   for f in os.listdir(data_dir) if f.find(".mat") != -1 ]
+
+    embeddings, name = [], []
+    for f in os.listdir(data_dir):
+        if f.find(".mat") != -1:
+            v = scipy.io.loadmat(data_dir + f)["data"].astype(np.float32)
+            if len(v) > 30:
+                v = v[:30]
+            embeddings.append(v)
+            name.append(f)
+            print (f, v.shape)
     max_label = len(embeddings)
     labels = []
     for i in range(len(embeddings)):
@@ -34,26 +42,24 @@ def read_data(params, data_dir="./data/", mlp=False):
         input = embeddings.reshape((len(embeddings), params["series_dim"] * params["input_dim"]))
     else:
         input = embeddings.reshape((len(embeddings), params["series_dim"], params["input_dim"]))
-    return split_data(input, labels), max_label
+    return split_data(input, labels), max_label, name
 
 class RNN(nn.Module):
     def __init__(self, params):
         super().__init__()
-
         self.rnn = nn.LSTM(params["input_dim"],
                            params["hidden_dim"],
                            num_layers=1,
                            bidirectional=False,
                            dropout=params["dropout"])
-
         self.fc = nn.Linear(params["hidden_dim"], params["output_dim"])
         self.log_softmax = nn.LogSoftmax()
         self.dropout = nn.Dropout(params["dropout"])
 
     def forward(self, input):
         # input = [sent len, batch size]
-        input = self.dropout(input)
-        output, (h, c) = self.rnn(input)
+        input = np.swapaxes(input, 0, 1)
+        output, (h, c) = self.rnn(self.dropout(input))
         return self.log_softmax(self.fc(self.dropout(h[-1])).squeeze(0))
 
 class MLP(nn.Module):
@@ -64,25 +70,32 @@ class MLP(nn.Module):
                       out_features=params["hidden_dim"]),
             nn.Dropout(params["dropout"]),
             nn.ReLU(),
+            nn.Linear(in_features=params["hidden_dim"], out_features=params["hidden_dim"]),
+            nn.Dropout(params["dropout"]),
+            nn.ReLU(),
             nn.Linear(in_features=params["hidden_dim"], out_features=params["output_dim"]),
         )
-        self.dropout = nn.Dropout(params["dropout"])
-        self.log_softmax = nn.LogSoftmax()
+        self.softmax = nn.Softmax()
 
     def forward(self, input):
-        return self.log_softmax(self.model(input).squeeze(0))
+        output = self.model(input).squeeze(0)
+        return self.softmax(output)
 
-def binary_accuracy(preds, y):
+def binary_accuracy(preds, y, max_label=-1, mat_conf=False):
     _, preds = preds.max(1)
     correct = (preds == y).float()
-    return correct.sum() / len(correct)
-
+    conf = None
+    if mat_conf:
+        conf = np.zeros((max_label, max_label))
+        for i in range(len(preds)):
+            conf[y[i], preds[i]] += 1
+    return correct.sum() / len(correct), conf
 
 def train(model, params, inputs, labels, optimizer, criterion, grad=False):
-    epoch_loss = 0
-    epoch_acc = 0
 
     model.train() if grad else model.eval()
+
+    epoch_loss, epoch_acc = 0, 0
     batch_size = params["batch_size"]
 
     for batch in range(int(len(inputs) / batch_size)):
@@ -90,17 +103,15 @@ def train(model, params, inputs, labels, optimizer, criterion, grad=False):
             optimizer.zero_grad()
 
         input = torch.tensor(inputs[batch * batch_size : batch * batch_size + batch_size])
-        if params["model"] == "LSTM":
-            input = np.swapaxes(input, 0, 1)
-
         label = torch.tensor(labels[batch * batch_size : batch * batch_size + batch_size])
 
-        predictions = model(input).squeeze(0)
-        
-        loss = criterion(predictions.double(), label)
-        acc = binary_accuracy(predictions.double(), label)
+        predictions = model(input)
+        predictions = torch.log(predictions).squeeze(0)
 
-        if grad:
+        loss = criterion(predictions.double(), label)
+        acc, _ = binary_accuracy(predictions.double(), label)
+
+        if not grad:
             loss.backward()
             optimizer.step()
 
@@ -110,28 +121,71 @@ def train(model, params, inputs, labels, optimizer, criterion, grad=False):
     return (epoch_loss / int(len(inputs) / batch_size),
             epoch_acc / int(len(inputs) / batch_size))
 
+def roc_accuracy(probs, y, roc, threshold):
+    probs = probs.detach().numpy()
+
+    val = (probs >= threshold).astype(np.int8)
+    for v, id in zip(val, y):
+        roc["true_pos"][id] += v[id]
+        roc["false_neg"][id] += int(v[id] == 0)
+
+    false_pos = (probs >= threshold).astype(np.int8)
+    false_pos[range(len(probs)), y.numpy()] = 0
+    false_pos = np.sum(false_pos, axis=0)
+
+    true_neg = (probs < threshold).astype(np.int8)
+    true_neg[range(len(probs)), y.numpy()] = 0
+    true_neg = np.sum(true_neg, axis=0)
+
+    roc["false_pos"] += false_pos
+    roc["true_neg"] += true_neg
+
+    return roc
+
+def compute_roc(model, x, y, max_label):
+    prob = model(torch.tensor(x))
+    _, count = np.unique(y, return_counts=True)
+    t_pos, f_neg, f_pos, t_neg = [], [], [], []
+    for threshold in range(0, 11):
+        roc = { "true_pos": np.zeros(max_label),
+                "false_neg": np.zeros(max_label),
+                "false_pos": np.zeros(max_label),
+                "true_neg": np.zeros(max_label) }
+        roc = roc_accuracy(prob, torch.tensor(y), roc, threshold / 10)
+        t_pos.append(roc["true_pos"] / count)
+        f_neg.append(roc["false_neg"] / count)
+        f_pos.append(roc["false_pos"] / (len(x) - count))
+        t_neg.append(roc["true_neg"] / (len(x) - count))
+
+    print ("true positive")
+    print (np.array(t_pos))
+    print ("false positive")
+    print (np.array(f_pos))
+    print ("false negative")
+    print (np.array(f_neg))
+    print ("true negative")
+    print (np.array(t_neg))
+
 def start_model():
 
     params = {
-        "model": "LSTM",
+        "model": "MLP",
         "series_dim": 16,
         "input_dim": 6 * 2 * 2,
-        "hidden_dim": 100,
+        "hidden_dim": 200,
         "n_epochs": 500,
         "batch_size": 50,
         "dropout": 0.
     }
 
-    (train_x, train_y, test_x, test_y), max_label = read_data(params, mlp=params["model"] == "MLP")
+    (train_x, train_y, test_x, test_y), max_label, name = read_data(params,
+                                                                    mlp=params["model"] == "MLP")
     params["output_dim"] = max_label
 
-    if params["model"] == "MLP":
-        model = MLP(params)
-    else:
-        model = RNN(params)
-
+    model = MLP(params) if params["model"] == "MLP" else RNN(params)
     criterion = nn.NLLLoss()
     optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
     for epoch in range(params["n_epochs"]):
 
         train_loss, train_acc = train(
@@ -142,7 +196,20 @@ def start_model():
             model, params, test_x, test_y, optimizer, criterion, grad=False)
         print ("TEST Epoch: {}, Loss: {}, Accuracy: {}".format(epoch, test_loss, test_acc))
 
-    print (test_y)
+    compute_roc(model, test_x, test_y, max_label)
+
+    prob = model(torch.tensor(test_x))
+    predictions = torch.log(prob).squeeze(0)
+    acc, conf = binary_accuracy(predictions.double(), torch.tensor(test_y),
+                                     max_label=max_label, mat_conf=True)
+    print ("final acc", acc)
+    """
+    print (name)
+    print (conf)
+    print (np.unique(test_y, return_counts=True))
+    """
+
+
 def main():
     start_model()
 
